2ca518987112595df543f78f8402836dd15f5185
Inish-Bashyal <inish.bashyal99@gmail.com>
2022-01-31 13:46:47 +0545

    Add faculty Panel completed

---
+++ src/controller/Facultycontroller.java
@@ -0,0 +1,93 @@
+package controller;
+
+import com.mysql.cj.xdevapi.PreparableStatement;
+
+import databaseConnection.databaseconnection;
+import model.Faculty;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import databaseConnection.databaseconnection;
+
+public class Facultycontroller {
+    databaseconnection db = new databaseconnection();
+
+    public int addFacultyPreparedStatement(Faculty faculty)
+    {
+
+        String query;
+
+        query ="insert into faculty( facFname, facLname,  gender,  phoneNo,  dateOfBirth,  username,password) values('"+
+                faculty.getfacFname() +"','" +
+                faculty.getfacLname()+"','"+
+                faculty.getGender()+"','"+
+                faculty.getPhoneNo()+"','"+
+                faculty.getDateOfBirth()+"','"+
+                faculty.getUsername()+"','"+
+                faculty.getPassword()+"');";
+
+        return db.manipulate(query);
+
+
+
+    }
+
+    // Login faculty
+    public Faculty loginFaculty(String username, String password) {
+        String query;
+        query = "select * from faculty where username = '" + username +
+                "' and password = '" + password + "';";
+
+        ResultSet rs = db.retrieve(query);
+        Faculty faculty = null;
+
+        try {
+            while (rs.next()) {
+                faculty = new Faculty();
+                faculty.setLoginId(rs.getInt("FactID"));
+                faculty.setfacFname(rs.getString("factFname"));
+                faculty.setfacLname(rs.getString("factLname"));
+                faculty.setGender(rs.getString("gender"));
+                faculty.setDateOfBirth(rs.getString("dateOfBirth"));
+                faculty.setUsername(rs.getString("username"));
+            }
+        } catch (Exception ex) {
+            System.out.println("Error" + ex);
+        }
+
+        return faculty;
+    }
+
+    public List<Faculty> getAllFacultys() {
+        String query;
+        query = "select * from faculty";
+        db = new databaseconnection();
+        ResultSet rs = db.retrieve(query);
+
+        List<Faculty> lstFacultys = new ArrayList<Faculty>();
+
+        try {
+            while (rs.next()) {
+                Faculty faculty = new Faculty();
+
+                faculty.setLoginId(rs.getInt("factID"));
+                faculty.setfacFname(rs.getString("factFname"));
+                faculty.setfacLname(rs.getString("facttLname"));
+                faculty.setGender(rs.getString("gender"));
+                faculty.setPhoneNo(rs.getString("phoneNo"));
+                faculty.setDateOfBirth(rs.getString("dateOfBirth"));
+                faculty.setUsername(rs.getString("username"));
+
+                lstFacultys.add(faculty);
+            }
+        } catch (Exception ex) {
+            System.out.println("Error" + ex);
+        }
+
+        return lstFacultys;
+    }
+
+
+}
---
+++ src/model/Faculty.java
@@ -0,0 +1,95 @@
+package model;
+
+public class Faculty {
+    private int LoginId;
+    private String facFname;
+    private String facLname;
+    private String gender;
+    private String phoneNo;
+    private String dateOfBirth;
+    private String username;
+    private String password;
+
+    public Faculty(){
+
+    }
+    public Faculty(String facFname, String facLname, String gender, String phoneNo, String dateOFBirth, String username,
+                   String password) {
+        this.facFname = facFname;
+        this.facLname = facLname;
+        this.gender = gender;
+        this.phoneNo = phoneNo;
+        this.dateOfBirth = dateOFBirth;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getLoginId(){
+        return LoginId;
+    }
+
+    public void setLoginId(int LoginId){
+        this.LoginId = LoginId;
+    }
+
+    public String getfacFname(){
+        return facFname;
+    }
+
+    public void setfacFname(String facFname){
+        this.facFname = facFname;
+    }
+
+    public String getfacLname(){
+        return facLname;
+    }
+
+    public void setfacLname(String facFname){
+        this.facLname = facLname;
+    }
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+
+
+
+}
